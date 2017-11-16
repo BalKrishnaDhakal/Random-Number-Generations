9a3857862b089d788df638306379fc3e56ecc9b1
BalKrishnaDhakal <bdhakal1@my.centennialcollege.ca>
2017-11-15 19:20:22 -0500

    Updated RandomIntegers class

--- Random Number Generations/src/RandomIntegers.java
+++ Random Number Generations/src/RandomIntegers.java
@@ -1,9 +1,19 @@
+import java.security.SecureRandom;
 
 public class RandomIntegers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		SecureRandom randomNumbers = new SecureRandom();
+		for(int counter=1; counter<=20; counter++) {
+			int face = 1 + randomNumbers.nextInt(6);// pick random numbers from 1 to 6
+			
+			System.out.printf("%d ",face);// Display generated values
+			// if the counter is divisible by 5, starts a new line to print--
+			if(counter %5==0)
+				System.out.println();
+		}
+			
 	}
 
 }
